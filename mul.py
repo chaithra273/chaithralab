823da36d8541733e899bb523ba8291b327e9b74e
chaithra manasa <190032068@kluniversity.in>
2021-10-25 22:33:40 +0530

    second commit

---
+++ mul.py
@@ -0,0 +1,10 @@
+Multiplication table(from 1 to 10) in Python
+
+number = int(input ("Enter the number of which the user wants to print the multiplication table: "))      
+count = 1    
+# we are using while loop for iterating the multiplication 10 times      
+print ("The Multiplication Table of: ", number)    
+while count <= 10:    
+    number = number * 1    
+    print (number, 'x', i, '=', number * count)    
+    count += 1    
